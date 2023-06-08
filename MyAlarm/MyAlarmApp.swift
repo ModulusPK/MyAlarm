1a5c06b4a564145c4ceb4decdf411a06875e5dfb
NasiruOjochide <nasiruojochide@gmail.com>
2023-06-08 06:54:26 +0100

    Initial Commit

---
+++ MyAlarm/MyAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAlarmApp.swift
+//  MyAlarm
+//
+//  Created by Danjuma Nasiru on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
